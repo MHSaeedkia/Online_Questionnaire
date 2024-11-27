9f3f69c71955784fb17042c88cc2cb60b5e05f0b
Mohammad Hasan Saeedkia <170322053+MHSaeedkia@users.noreply.github.com>
2024-11-27 16:53:55 +0330

    project structure .

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash 
+
+
+mod=$(ls|grep go.mod)
+if [ "$mod" != "go.mod" ] ; then 
+    cd ..
+    go mod init Golang-bc8-quera/web_crawler
+    go mod tidy
+fi 
+
+
+go build -o web-crawler ./cmd/app/crawler.go
+
+
