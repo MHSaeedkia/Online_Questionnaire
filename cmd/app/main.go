5ceac30753cd52bf2013bc0903c28d03704cc292
Sahar Hallaji <saharhallaji.dev@gmail.com>
2024-11-30 13:58:30 +0330

    feat(swagger-fiber): swagger and fiber are added to the project, also an endpoint to check the project version has added to the project.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
 	"log"
 	config "online-questionnaire/configs"
 	"online-questionnaire/internal/db"
+	"online-questionnaire/internal/handlers"
 )
 
+// @title			online Questionnaire
+// @version		1.0
+// @description Questionnaire Management System API
 func main() {
 	cfg, err := config.LoadConfig("./configs/")
 	if err != nil {
@@ -19,4 +25,23 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(DB, "is connected successfully")
+
+	// Initialize Fiber app
+	app := fiber.New()
+
+	// Serve the Swagger JSON file at /swagger/doc.json
+	app.Get("/swagger/doc.json", func(c *fiber.Ctx) error {
+		return c.SendFile("./docs/swagger.json") // Adjust the path if necessary
+	})
+
+	// Serve the Swagger UI at /swagger
+	app.Get("/swagger/*", swagger.HandlerDefault)
+
+	// Handlers
+	app.Get("/version", handlers.GetVersion)
+
+	// App port
+	if err := app.Listen(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,29 @@
 package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"os"
+)
+
+//	@title			Questionnaire API
+//	@version		1.0
+//	@description	This is a sample API for the questionnaire service.
+//	@BasePath		/api
+
+// @Summary		Get version of the API
+// @Description	Returns the version of the API
+// @Tags			version
+// @Accept			json
+// @Produce		json
+// @Success		200	{string}	string	"ok"
+// @Router			/version [get]
+func GetVersion(c *fiber.Ctx) error {
+	version := os.Getenv("PROJECT_VERSION")
+	if version == "" {
+		version = "1.0.0"
+	}
+
+	return c.JSON(fiber.Map{
+		"version": version,
+	})
+}
