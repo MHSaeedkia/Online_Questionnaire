ce0fdfbfac667efbbcf9fdcdba6633b7011eb1e5
hamed.maleki <hamed.maleki@snapp.cab>
2024-11-30 16:16:34 +0330

    Add functions to validate email, national id and generate jwt token

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,14 +5,16 @@ import (
 	"log"
 	config "online-questionnaire/configs"
 	"online-questionnaire/internal/db"
+	"online-questionnaire/internal/jwtgen"
 )
 
 func main() {
-	cfg, err := config.LoadConfig("./configs/")
+	cfg, err := config.LoadConfig("../../configs/")
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	token, err := jwtgen.GenerateJWTToken("hamed", cfg)
+	fmt.Println(token)
 	//database connect
 	DB, err := db.NewConnection(&cfg.Database)
 	if err != nil {
